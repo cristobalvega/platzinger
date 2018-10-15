bc7ae1a456471567ec687a4b5e3365855d5a84a2
cristobalvega <crelvegalu@ittepic.edu.mx>
2018-10-15 13:17:50 -0600

    ¿Cómo usar tipos de datos con typescript

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    console.log('HomeComponent');
+    const c = 1;
+    const b = 2;
+    console.log('c+ b = ', c + b);
+
+    const e = '1';
+    const f = '2';
+    console.log('e + f =', e + f);
+
+    const g = true;
+    const h: object  = {};
+
+    console.log('g = ', g);
+    console.log('h = ', h);
+
+    const i = [c, b, e, f, h];
+    console.log('el arreglo', i);
+
+    const j: boolean [] = [true, g];
+    console.log('el arreglo j = ', j);
+
+    const k: object[] = [{}, h];
+    console.log('el valor de k =', k);
+
+    const l: any[] = [1, 'aoe', {}, []];
+    console.log('el valor dl arreglo l = ', l);
+  }
 
   ngOnInit() {
   }
