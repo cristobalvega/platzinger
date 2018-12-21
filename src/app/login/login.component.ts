546d88b7d2ddcec2d17eb803c6c9c419d241d0e7
Vega <CVega@Watlow.com>
2018-12-21 15:51:52 -0600

    saahdkhsd

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthenticationService} from '../services/authentication.service';
+import {assertLessThan} from '@angular/core/src/render3/assert';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
   operation = 'login';
-  constructor() { }
+  email: string = null;
+  password: string = null;
+
+  constructor(private authenticationService: AuthenticationService) { }
 
   ngOnInit() {
+  }
+
+  login() {
+    this.authenticationService.loginWithEmail(this.email, this.password).then((data) => {
+      alert('Loggeado correctamente');
+      console.log('el data | login() = ', data);
+    }).catch((error) => {
+      alert('Ocurrio un error');
+      console.log('el error | login() = ', error);
+    });
+  }
+
+  register() {
+    this.authenticationService.registerWithEmail(this.email, this.password).then((data) => {
+      alert('Registrado correctamente');
+      console.log('el data | register() = ', data);
+    }).catch((error) => {
+      alert('Ocurrio un error');
+      console.log('el error | register() = ', error);
+    });
   }
 
 }
